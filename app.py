e6ccccfb590414f69364ca57e7d33e40fcfe2716
yury_zhelnin <yvzhelnin@gmail.com>
2020-07-16 17:37:36 +0300

    updated code

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return 'Hello with love from ' + os.environ['HOSTNAME'] + '!'
+    return 'Hello with love from ' + os.environ['HOSTNAME'] + '!\n'
 
 @app.route("/health")
 def health_check():
-    return '{"status": "OK"}'
+    return '{"status": "OK"}\n'
 
 @app.route("/version")
 def version():
-    return '{"version": 0.6}'
+    return '{"version": 0.6}\n'
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port = '80')
